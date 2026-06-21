3159d06cb02aa223f468f294e015dffc5531ef88
lakpalama7 <sherpasvcet@gmail.com>
2026-06-21 19:07:52 +0300

    pandas cleaning missing values

---
+++ pandas_datacleaning.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+
+d = {'First Score': [100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score': [np.nan, 40, 80, 98]}
+df = pd.DataFrame(d)
+print(df)
+
+# missing values
+print(df.isnull().sum())
+
+bool_v = pd.isnull(df['First Score'])
+
+print(df[bool_v])
+print(df.iloc[2,0])
+
+print(df.isna().sum())
+
+# check non-missing values using notnull()
+
+print(df.notnull())
+print(df.notnull().sum())
+
+# filter non-missing values
+n_null = pd.notnull(df['First Score'])
+print(df[n_null]['First Score'])
+
+print(df)
+
+# fill missiing value 
+df.fillna(0, inplace=True)
+print(df)
+
+d = {'First Score': [100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score': [np.nan, 40, 80, 98]}
+df = pd.DataFrame(d)
+print(df)
+
+# fill with custom value
+df['First Score']=df['First Score'].fillna(0, inplace=True)
+print(df)
+
+df['Second Score']=df['Second Score'].fillna(df['Second Score'].agg('mean'),inplace=True)
+print(df)
+
+df['Third Score'] = df['Third Score'].fillna(df['Third Score'].agg('min'), inplace=True)
+print(df['Third Score'])
+
+d = {'First Score': [100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score': [np.nan, 40, 80, 98]}
+df = pd.DataFrame(d)
+print(df)
+
+# replace nan value
+df = df.replace(to_replace=np.nan, value=99)
+print(df)
+
+
+dict = {'First Score': [100, 90, np.nan, 95],
+        'Second Score': [30, np.nan, 45, 56],
+        'Third Score': [52, 40, 80, 98],
+        'Fourth Score': [np.nan, np.nan, np.nan, 65]}
+df = pd.DataFrame(dict)
+print(df)
+
+# drop rows with at least one NULL value
+df.dropna(inplace=True)
+print(df)
+
+# dropping rows having all value null
+
+dict = {'First Score': [100, np.nan, np.nan, 95],
+        'Second Score': [30, np.nan, 45, 56],
+        'Third Score': [52, np.nan, 80, 98],
+        'Fourth Score': [np.nan, np.nan, np.nan, 65]}
+df = pd.DataFrame(dict)
+print(df)
+df.dropna(how='all', inplace=True)
+print(df)
+
+# dropping cols with at least one null value
+dict = {'First Score': [100, np.nan, np.nan, 95],
+        'Second Score': [30, np.nan, 45, 56],
+        'Third Score': [52, np.nan, 80, 98],
+        'Fourth Score': [60, 67, 68, 65]}
+df = pd.DataFrame(dict)
+print(df)
+print(df.dropna(axis=1))
+
+# drop rows with missing values 
+print(df.dropna(how='any'))
